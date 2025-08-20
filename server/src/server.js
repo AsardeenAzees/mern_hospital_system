bc861fb63e32298558083fbb8ce5d5647dc66bb3
kijan <mohamedkijan321@gmail.com>
2025-08-20 06:16:26 +0530

    fdf

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { connectDB } from './config/db.js';
 import app from './app.js';
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 connectDB().then(() => {
     console.log('Database connected, starting server...');
@@ -10,7 +10,7 @@ connectDB().then(() => {
         console.log(`API running on http://localhost:${port}`);
         console.log('Server is listening on port:', port);
     });
-    
+
     server.on('error', (error) => {
         console.error('Server error:', error);
     });
